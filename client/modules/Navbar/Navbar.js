216be0048e53dc9756c8a3d46e4a5d9c66849bdf
Ishita Kothari <ishitadkothari@gmail.com>
2016-09-29 12:39:49 +0530

    initial commit

---
+++ client/modules/Navbar/Navbar.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react'
+import {Navbar, NavItem, Nav} from 'react-bootstrap'
+
+
+export default class Demo extends Component{
+  render(){
+    return(
+      <div>
+        <Navbar inverse>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">React-Bootstrap</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem eventKey={1} href="#">Link</NavItem>
+              <NavItem eventKey={2} href="#">Link</NavItem>
+            </Nav>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="#">Link Right</NavItem>
+              <NavItem eventKey={2} href="#">Link Right</NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <br />
+        <nav className="navbar navbar-inverse">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="#">WebSiteName</a>
+            </div>
+            <div className="collapse navbar-collapse" id="myNavbar">
+              <ul className="nav navbar-nav">
+                <li className="active"><a href="#">Home</a></li>
+                <li><a href="#">Page 1</a></li>
+                <li><a href="#">Page 2</a></li>
+                <li><a href="#">Page 3</a></li>
+              </ul>
+              <ul className="nav navbar-nav navbar-right">
+                <li><a href="#"><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                <li><a href="#"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    )
+  }
+}
---
+++ client/modules/demo/demo.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import Navbar from '../Navbar/Navbar'
+
+export default class Demo extends Component{
+  render(){
+    return(
+      <div>
+        <Navbar />
+        hello world!!!
+      </div>
+    )
+  }
+}
