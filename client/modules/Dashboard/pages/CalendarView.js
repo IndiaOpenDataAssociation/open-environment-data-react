b556724c1d534c31e601ed259cd5ed46af9c2ec2
Ishita Kothari <ishitadkothari@gmail.com>
2016-12-10 11:07:11 +0530

    date-change

--- client/modules/Dashboard/pages/CalendarView.js
+++ client/modules/Dashboard/pages/CalendarView.js
@@ -3,22 +3,24 @@ import DropdownButton from 'react-bootstrap/lib/DropdownButton'
 import MenuItem from 'react-bootstrap/lib/MenuItem'
 // import data from '../components/pollution.json'
 import superagent from 'superagent'
+import _ from 'lodash'
 
-let heatmap, displaydate = [], time=[], array = [];
-export default class CalendarView extends Component{
-  constructor(props){
+
+let heatmap, displaydate = [], time = [], array = [], dateUniq = [];
+export default class CalendarView extends Component {
+  constructor(props) {
     super(props)
     this.state = {
       dailyData: [],
-      dailyDataLoading : true,
+      dailyDataLoading: true,
       noDailyData: false
     }
 
-    let lte = new Date().getTime()/1000
+    let lte = new Date().getTime() / 1000
     var today = new Date()
-    var gte= new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000).getTime()/1000;
-    superagent.get('https://openenvironment.p.mashape.com/all/public/analytics/range/'+this.props.markerId+'?gte='+gte+'&lte='+lte).set('X-Mashape-Key', 'SPmv0Z46zymshRjsWckXKsA09OBrp14RCeSjsniWIpRk6llTuk').end(function (err, res) {
-      if(res.statusText != "Not Found") {
+    var gte = new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000).getTime() / 1000;
+    superagent.get('https://openenvironment.p.mashape.com/all/public/analytics/range/' + this.props.markerId + '?gte=' + gte + '&lte=' + lte).set('X-Mashape-Key', 'SPmv0Z46zymshRjsWckXKsA09OBrp14RCeSjsniWIpRk6llTuk').end(function (err, res) {
+      if (res.statusText != "Not Found") {
         this.setState({dailyData: res.body})
         this.setState({dailyDataLoading: false})
         array = []
@@ -28,17 +30,19 @@ export default class CalendarView extends Component{
           var date = a.getDate();
           var hour = a.getHours();
           let min = a.getMinutes();
-          if(min < 10){
-            min = '0'+min
+          if (min < 10) {
+            min = '0' + min
           }
           let Time = hour + ':' + min
-          if(hour >= 12){
-            time.push(Time+'pm')
+          if (hour >= 12) {
+            time.push(Time + 'pm')
           }
-          else{
-            time.push(Time+'am')
+          else {
+            time.push(Time + 'am')
           }
-          displaydate.push(date + 'th')
+          if (displaydate.indexOf(date+'th') === -1) {
+            displaydate.push(date+'th')
+          }
           array.push([hour, date, e.aqi])
         })
 
@@ -86,33 +90,33 @@ export default class CalendarView extends Component{
             gridLineColor: 'transparent',
           },
 
-          colors : ['#0C6657', '#1BCCAC', '#60E5D7'],
-          colorAxis : {
-            dataClassColor : 'category',
-            dataClasses : [{
-              to : 500,
+          colors: ['#0C6657', '#1BCCAC', '#60E5D7'],
+          colorAxis: {
+            dataClassColor: 'category',
+            dataClasses: [{
+              to: 500,
             }, {
-              from : 170,
-              to : 340
+              from: 170,
+              to: 340
             }, {
-              from : 170,
+              from: 170,
             }
             ]
           },
 
           // colorAxis: {
-            // min: 0,
-            // max: 600,
-            // stops: [
-            //   [0.1,'green'],
-            //   [0.5,'orange'],
-            //   [0.9,'red']
-            // ],
-            // minColor: Highcharts.getOptions().colors[1],
-            // maxColor: Highcharts.getOptions().colors[0]
+          // min: 0,
+          // max: 600,
+          // stops: [
+          //   [0.1,'green'],
+          //   [0.5,'orange'],
+          //   [0.9,'red']
+          // ],
+          // minColor: Highcharts.getOptions().colors[1],
+          // maxColor: Highcharts.getOptions().colors[0]
           // },
 
-          legend:{
+          legend: {
             enabled: false,
           },
           series: [
@@ -133,39 +137,40 @@ export default class CalendarView extends Component{
 
         })
       }
-      else{
+      else {
         this.setState({noDailyData: true})
       }
     }.bind(this))
   }
 
-  componentDidMount(){
+  componentDidMount() {
 
   }
 
 
-  render(){
+  render() {
 
-    return(
+    return (
       <div >
         <div className="analytics-div">
           <div className="analytics-chart">
 
             {
               this.state.noDailyData == false
-              ?
-                (
-                this.state.dailyDataLoading
                 ?
-                  <div style={{height: '300px'}}>
-                    <i className="fa fa-spinner fa-spin" style={{fontSize: '30px', color: '#00B3BF', lineHeight: '300px'}}></i>
-                  </div>
-                :
-                  <div className="heatmap" ref="heatmap">
+                (
+                  this.state.dailyDataLoading
+                    ?
+                    <div style={{height: '300px'}}>
+                      <i className="fa fa-spinner fa-spin"
+                         style={{fontSize: '30px', color: '#00B3BF', lineHeight: '300px'}}></i>
+                    </div>
+                    :
+                    <div className="heatmap" ref="heatmap">
 
-                  </div>
+                    </div>
                 )
-              :
+                :
                 <div style={{fontSize: '30px', color: '#00B3BF', lineHeight: '400px'}}>
                   No data available
                 </div>
@@ -193,7 +198,10 @@ export default class CalendarView extends Component{
               <MenuItem eventKey="1">AQI</MenuItem>
             </DropdownButton>
             <p>
-              An air quality index (AQI) is a number used by government agencies to communicate to the public how polluted the air currently is or how polluted it is forecast to become. As the AQI increases, an increasingly large percentage of the population is likely to experience increasingly severe adverse health effects.
+              An air quality index (AQI) is a number used by government agencies to communicate to the public how
+              polluted the air currently is or how polluted it is forecast to become. As the AQI increases, an
+              increasingly large percentage of the population is likely to experience increasingly severe adverse health
+              effects.
             </p>
           </div>
         </div>
