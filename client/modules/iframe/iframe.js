220107eda9fed314a69952248206d1d8db94ceac
jimish <jimishparekh123@gmail.com>
2017-03-02 13:35:27 +0530

    changed styling of iframe

--- client/modules/iframe/iframe.js
+++ client/modules/iframe/iframe.js
@@ -56,6 +56,7 @@ export default class Iframe extends Component {
 
     this.getDynamicClassName = this.getDynamicClassName.bind(this)
     this.changeData = this.changeData.bind(this)
+    this.createInfoTable = this.createInfoTable.bind(this)
 
   }
 
@@ -95,14 +96,152 @@ export default class Iframe extends Component {
       });
 
     axios.post('/iframe', {"devices": this.devices}, config).then(function (response) {
+      console.log("iframe response :",response);
+
+
       if (response) {
-        this.setState({iframeData: response.data})
-        this.setState({activeTab: this.state.iframeData[0].label})
+
+        if(response.status == 200 && response.data.length>0){
+          this.setState({iframeData: response.data})
+          this.setState({activeTab: this.state.iframeData[0].label})
+        } else {
+          console.log("came here 1");
+          var currentTime = new Date().getTime();
+          currentTime = currentTime / 1000;
+          currentTime = currentTime - 1800;
+          var iframeLocalData = [
+            {
+              "_id": "58ad4d4c884666000b2763a2",
+              "payload": {
+                "d": {
+                  "t": currentTime,
+                  "g1": 515,
+                  "g2": 0,
+                  "p2": 24,
+                  "p1": 2.4,
+                  "temp": 31.63,
+                  "hum": 18.47,
+                  "noise": [
+                    33,
+                    70,
+                    28,
+                    31,
+                    67
+                  ]
+                }
+              },
+              "deviceId": "OZ_PARTICLE_007",
+              "deviceType": "POLLUDRON_PRO",
+              "aqi": 24,
+              "aqikey": "p2",
+              "label": "Dhordo",
+              "type": "POLLUDRON_PRO",
+              "desc": "Real time Air Quality Level of Dhordo,Kutch - Rannotsav."
+            },
+            {
+              "_id": "58b6b4535aafa7000b202834",
+              "payload": {
+                "d": {
+                  "t": currentTime,
+                  "g1": 608,
+                  "g2": 0,
+                  "p2": 56.8,
+                  "p1": 16,
+                  "temp": 31.54,
+                  "hum": 40.77,
+                  "noise": [
+                    244,
+                    255,
+                    35,
+                    56,
+                    0
+                  ]
+                }
+              },
+              "deviceId": "OZ_PARTICLE_002",
+              "deviceType": "POLLUDRON_PRO",
+              "aqi": 56,
+              "aqikey": "p2",
+              "label": "Somnath",
+              "type": "POLLUDRON_PRO",
+              "desc": "Real time Air Quality Level of Somnath Temple."
+            }
+          ];
+
+          this.setState({iframeData: iframeLocalData})
+          this.setState({activeTab: this.state.iframeData[0].label})
+        }
+        
 
       }
     }.bind(this))
       .catch(function (error) {
         console.log(error);
+        console.log("came here 2");
+        var currentTime = new Date().getTime();
+          currentTime = currentTime / 1000;
+          currentTime = currentTime - 1800;
+          var iframeLocalData = [
+            {
+              "_id": "58ad4d4c884666000b2763a2",
+              "payload": {
+                "d": {
+                  "t": currentTime,
+                  "g1": 515,
+                  "g2": 0,
+                  "p2": 24,
+                  "p1": 2.4,
+                  "temp": 31.63,
+                  "hum": 18.47,
+                  "noise": [
+                    33,
+                    70,
+                    28,
+                    31,
+                    67
+                  ]
+                }
+              },
+              "deviceId": "OZ_PARTICLE_007",
+              "deviceType": "POLLUDRON_PRO",
+              "aqi": 24,
+              "aqikey": "p2",
+              "label": "Dhordo",
+              "type": "POLLUDRON_PRO",
+              "desc": "Real time Air Quality Level of Dhordo,Kutch - Rannotsav."
+            },
+            {
+              "_id": "58b6b4535aafa7000b202834",
+              "payload": {
+                "d": {
+                  "t": currentTime,
+                  "g1": 608,
+                  "g2": 0,
+                  "p2": 56.8,
+                  "p1": 16,
+                  "temp": 31.54,
+                  "hum": 40.77,
+                  "noise": [
+                    244,
+                    255,
+                    35,
+                    56,
+                    0
+                  ]
+                }
+              },
+              "deviceId": "OZ_PARTICLE_002",
+              "deviceType": "POLLUDRON_PRO",
+              "aqi": 56,
+              "aqikey": "p2",
+              "label": "Somnath",
+              "type": "POLLUDRON_PRO",
+              "desc": "Real time Air Quality Level of Somnath Temple."
+            }
+          ];
+
+          this.setState({iframeData: iframeLocalData})
+          this.setState({activeTab: this.state.iframeData[0].label})
       });
 
     // superagent.get('https://openenvironment.p.mashape.com/fields').set('X-Mashape-Key', 'SPmv0Z46zymshRjsWckXKsA09OBrp14RCeSjsniWIpRk6llTuk').end(function (err, res) {
@@ -127,7 +266,8 @@ export default class Iframe extends Component {
       fields: [],
       iframeData: [],
       limits: [],
-      activeTab: 'Dhordo'
+      activeTab: 'Dhordo',
+      infoItem: []
     }
   }
 
@@ -147,6 +287,19 @@ export default class Iframe extends Component {
     return className
   }
 
+  createInfoTable(data, key){
+    let infoItem = []
+    if(data){
+       data.map((dataItem) => {
+        if (dataItem.fkey == key) {
+          dataItem.range.map((rangeItem, index) => {
+            infoItem.push(rangeItem)
+          })
+        }
+      })
+      this.setState({infoItem})
+    }
+  }
   changeData(data) {
     this.setState({
       activeTab: data
@@ -173,7 +326,7 @@ export default class Iframe extends Component {
                           return (
 
                             <div key={e.label}>
-                              <h3>Air Quality Index - {e.label}</h3>
+                              <h3>Weather/Environment Data - {e.label}</h3>
                               <div className="iframe-body" key={e.label}>
                                 <div className="gas-list">
                                   <ul className="list-inline">
@@ -186,7 +339,7 @@ export default class Iframe extends Component {
                                             <li>
                                               <h4
                                                 className={this.getDynamicClassName(this.state.limits, 'aqi', e.aqi)}>{e.aqi}</h4>
-                                              <p>AQI</p>
+                                              <p onClick={() => {this.createInfoTable(this.state.limits, 'aqi')}}>Air Quality Index</p>
                                             </li>
                                             :
                                             null
@@ -196,7 +349,7 @@ export default class Iframe extends Component {
                                           <li>
                                             <h4
                                               className={this.getDynamicClassName(this.state.limits, 'aqi', e.aqi)}>{e.aqi}</h4>
-                                            <p>AQI</p>
+                                            <p onClick={() => {this.createInfoTable(this.state.limits, 'aqi')}}>Air Quality Index</p>
                                           </li>
                                         )
 
@@ -220,7 +373,7 @@ export default class Iframe extends Component {
                                                     })
                                                   }
                                                 </h4>
-                                                <p>
+                                                <p onClick={() => {this.createInfoTable(this.state.limits, key)}}>
                                                   {
                                                     fields.map(function (e) {
                                                       if (e.fkey == key)
@@ -228,6 +381,7 @@ export default class Iframe extends Component {
                                                     })
                                                   }
                                                 </p>
+
                                               </li>
                                             )
                                           }
@@ -251,7 +405,7 @@ export default class Iframe extends Component {
                                                     })
                                                   }
                                                 </h4>
-                                                <p>
+                                                <p onClick={() => {this.createInfoTable(this.state.limits, key)}}>
                                                   {
                                                     fields.map(function (e) {
                                                       if (e.fkey == key)
@@ -269,6 +423,38 @@ export default class Iframe extends Component {
                                   </ul>
                                 </div>
                               </div>
+
+                              {
+                                this.state.infoItem.length > 0
+                                  ?
+                                  <div className="gas-indicator">
+                                    <table className="gas-indicator-table">
+                                      <tbody>
+                                      <tr className="gas-info">
+                                        <td><span></span></td>
+                                        <td className="good"><span>Good</span></td>
+                                        <td className="satisfactory"><span >Satisfactory</span></td>
+                                        <td className="moderate"><span >Moderate</span></td>
+                                        <td className="poor"><span>Poor</span></td>
+                                        <td className="vpoor"><span>Very Poor</span></td>
+                                        <td className="severe"><span>Severe</span></td>
+                                      </tr>
+                                      <tr>
+                                        {
+                                          this.state.infoItem.map((e) => {
+                                            return (
+                                              <td className="text-right">{e}</td>
+                                            )
+                                          })
+
+                                        }
+                                      </tr>
+                                      </tbody>
+                                    </table>
+                                  </div>
+                                  :
+                                  null
+                              }
                               {/*<div className="description">
                                <p>
                                <i className="fa fa-info-circle"></i>
